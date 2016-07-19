34e079a6c0529f34b584e34fb4a816d513afce7a
jyuill <john@fig4.com>
2016-07-19 13:53:20 -0700

    more logistic regression, partially translated into markdown

--- wk3-logistic-regression1.R
+++ wk3-logistic-regression1.R
@@ -27,5 +27,43 @@ tapply(predictTrain,qualityTrain$PoorCare,mean)
 ## - for all true good care cases (0), we predict ave. probability of ~0.19
 ## - good because we're predicting higher probabily for actual poor care cases
 
-
+### Threshold
+## set threshold over which prediction is determined
+## if no preference use t=0.5
 ## threshold examples using confusion tables
+## sensitivity = TP / (TP + FN) True positive rate
+## specificity = TN / (TN + FP) True negative rate
+
+table(qualityTrain$PoorCare,predictTrain>0.5)
+## sensitivity - True pos: (based on numbers in confusion matrix)
+10/25
+## specificity - True neg:
+70/74
+## conclusion: much more accurate at identifying negatives, not so good at positives
+## try again with higher threshold
+table(qualityTrain$PoorCare,predictTrain>0.7)
+## sensitivity - True pos rate:
+8/25
+## specificity - True neg rate:
+73/74
+## conclusion: even better at identifying negatives, worse with positives
+## try again with lower threshold
+table(qualityTrain$PoorCare,predictTrain>0.2)
+## sensitivity - True pos rate:
+16/25
+## specificity -True neg rate:
+54/74
+## conclusion: still leans toward negatives, but more balanced
+
+### ROC CURVES to determine where to set threshold, based on priorities
+library(ROCR)
+ROCRpred <- prediction(predictTrain, qualityTrain$PoorCare)
+ROCRperf <- performance(ROCRpred,"tpr","fpr")
+plot(ROCRperf)
+## chart above shows combinations of True positive rate and False positive rate
+## at different thresholds - but doesn't tell you threshold values
+plot(ROCRperf,colorize=TRUE)
+## chart above associates colors in line with different threshold values
+plot(ROCRperf,colorize=TRUE, print.cutoffs.at=seq(0,1,0.1),text.adj=c(-0.2,1.7))
+## using chart above can determine threshold to use, depending on
+## preferences as decision-maker
